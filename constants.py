0c6cdcfa33990923c39638f8a5929942d0630492
cháo lòng <111640287+chaos-long@users.noreply.github.com>
2023-07-25 20:52:20 +0700

    Add files via upload

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 # Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
 # and rename this file to constants.py.
-APIKEY = "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"
+APIKEY = "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"
