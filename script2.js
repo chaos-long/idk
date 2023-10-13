340df6f0b76ed8c4a01a712e8af78704301faa4d
cháo lòng <111640287+chaos-long@users.noreply.github.com>
2023-10-13 16:19:51 +0700

    Update script2.js

--- script2.js
+++ script2.js
@@ -6,11 +6,24 @@ const carouselChildrens = [...carousel.children];
 
 let isDragging = false, startX, startScrollLeft, timeoutId;
 
-// let cardPe2
+let cardPerView = Math.round(carousel.offsetWidth/ firstCardWidth);
+let cc = carouselChildrens.slice(0);
+console.log(cardPerView);
+console.log(cc.length);
+if (cardPerView < cc.length) {
+    carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+        carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+    });
+
+    carouselChildrens.slice(0, cardPerView).forEach(card => {
+        carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+
+    });
+}
 
 arrowBtns.forEach(btn => {
     btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+        carousel.scrollLeft += btn.id === "left" || btn.id === "left2" || btn.id === "left3" ? -firstCardWidth : firstCardWidth;
 
     })
 });
@@ -29,29 +42,29 @@ const dragStop = () =>{
     carousel.classList.remove("dragging");
 }
 
-const autoPlay = () =>{
-    if(window.innerWidth < 800) return;
-    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
-}
-autoPlay();
-const infiniteScroll = () => {
-    if(carousel.scrollLeft === 0){
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.scrollWidth - (2* carousel.offsetWidth);
-        carousel.classList.remove("no-transition");
-    }
-    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.offsetWidth;
-        carousel.classList.remove("no-transition");
-    }
+// const autoPlay = () =>{
+//     if(window.innerWidth < 800) return;
+//     timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
+// }
+// autoPlay();
+// const infiniteScroll = () => {
+//     if(carousel.scrollLeft === 0){
+//         carousel.classList.add("no-transition");
+//         carousel.scrollLeft = carousel.scrollWidth - (2* carousel.offsetWidth);
+//         carousel.classList.remove("no-transition");
+//     }
+//     else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+//         carousel.classList.add("no-transition");
+//         carousel.scrollLeft = carousel.offsetWidth;
+//         carousel.classList.remove("no-transition");
+//     }
 
-    clearTimeout(timeoutId);
-    if(!wrapper.matches(":hover")) autoPlay();
-}
+//     clearTimeout(timeoutId);
+//     if(!wrapper.matches(":hover")) autoPlay();
+// }
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
 wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
-carousel.addEventListener("scroll", infiniteScroll);
-wrapper.addEventListener("mouseleave", autoPlay);
+// carousel.addEventListener("scroll", infiniteScroll);
+// wrapper.addEventListener("mouseleave", autoPlay);
