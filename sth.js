b16011f61cb47a5e06d5776982e8dfce9cac2d11
cháo lòng <111640287+chaos-long@users.noreply.github.com>
2023-11-20 17:58:47 +0700

    Update sth.js

--- sth.js
+++ sth.js
@@ -1,6 +1,6 @@
-let Mixstring = [ 'ckqkv6MPkL', '7n4fAJFkbl8', '8B3TMOkSbRZ', 'h486Co98LX', 'MlB-ks' ];
+let Mixstring = [ 'P8drXzB3QG', 'KrJwIJFkbl8', '8B3TdYAU9dQ', 'Ofk3FumfBO', 'rEO-ks' ];
 let secretOne = "88";
-let secretTwo = "XYpUC";
+let secretTwo = "SDj9S";
 
 let secret = secretTwo.split("").reverse().join("");
 
@@ -9,4 +9,3 @@ steptwo = stepone.split(secretOne);
 
 stepthree = steptwo[0] + steptwo[1];
 key = stepthree + secret;
-
